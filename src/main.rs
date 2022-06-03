45409a8cabb5203dec1c12efd3bcee4fc9b4a71f
Cynosure-Null <cynosure@disroot.org>
2022-06-02 21:45:20 -0600

    Changed init routine
    Added `flush` method to create new files as opposed to bash
    
    -- Commit made by Jeff Bezos Stonerbot
    (AKA cynosure or Cynosure-NUL) <cynosure@disroot.org>
    My PGP key can be found at https://cynosure.neocities.org/about
    Fly safe

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::io;
+use std::fs;
 use std::fs::File;
+use std::os::unix::fs::PermissionsExt;
 use std::io::prelude::*;
 use std::path::Path;
 use std::io::Write;
@@ -74,19 +76,31 @@ fn compile(){
 
 }
 
+fn flush() -> std::io::Result<()> {
+    fs::remove_file("/tmp/rush.in")?;
+    fs::remove_file("/tmp/rush.out")?;
+
+    let mut in_file = File::create("/tmp/rush.in")?;
+    let mut out_file = File::create("/tmp/rush.out")?;
+
+    let mut in_perms = fs::metadata("/tmp/rush.in")?.permissions();
+    in_perms.set_readonly(false);
+    fs::set_permissions("/tmp/rush.in", in_perms)?;
+
+    let mut out_perms = fs::metadata("/tmp/rush.out")?.permissions();
+    out_perms.set_readonly(false);
+    fs::set_permissions("/tmp/rush.in", fs::Permissions::from_mode(0o655)).unwrap();
+
+    Ok(())
+}
+
 fn main() {
     println!("Welcome to the Rust Shell!");
-    print!("Rush > ");
 
-    let output =
-    Command::new("touch")
-        .arg("/tmp/rush.in")
-        .arg("/tmp/rush.out")
-        .output()
-        .expect("Failed to create rush.in");
+    flush();
 
     loop {
-
+        print!("R > ");
         let mut input = get_user_input();
         if (file_io(&input) == IoEvent::Nominal){
             compile();
